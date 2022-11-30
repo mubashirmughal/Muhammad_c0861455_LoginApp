5391b39fad09a02d37d7d955b309f2d96227caf6
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-29 20:06:15 -0500

    added variable in homeactivity

--- app/src/main/java/com/example/loginapp/HomeActivity.java
+++ app/src/main/java/com/example/loginapp/HomeActivity.java
@@ -1,14 +1,20 @@
 package com.example.loginapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+    private EditText usernameTxt;
+    private EditText passwordTxt;
+
+    private static String username;
+    private static String password;
 }
